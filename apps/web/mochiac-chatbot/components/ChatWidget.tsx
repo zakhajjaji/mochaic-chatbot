331a2373f490ad820aee60603ab328c37368596f
Zak <zak.hajjaji@icloud.com>
2025-10-30 10:22:01 +0000

    Feat(chatwidget): Scaffold state and UI shell. added modal states

--- apps/web/mochiac-chatbot/components/ChatWidget.tsx
+++ apps/web/mochiac-chatbot/components/ChatWidget.tsx
@@ -1,31 +1,44 @@
 "use client";
-
 import { cn } from "@/lib/utils";
 import { useState, useEffect, useMemo, useCallback, useRef } from "react";
 
-type chatMessage = {
-    role: "user" | "assistant" | "system";
-    content: string;
+export type ChatMessage = { 
+    role: "user" | "assistant" | "system"; 
+    content: string; 
     timestamp: Date;
-    id: string;
-    messageType: "text" | "image" | "audio" | "video" | "file";
+    id: string; 
+    messageType: "text" | "image" | "audio" | "video" | "file"; 
 }
 
-type chatMessageProps = {
-    message: chatMessage;
-    isUser: boolean;
+const ChatMessage = ({ message}: {message: ChatMessage}) => {
+  const isUser = message.role === "user"; 
+  return (
+    <div className={cn("flex flex-col gap-2", isUser ? "items-end" : "items-start")}>
+      <h1>Chat Message</h1>
+      <p>{message.content}</p>
+      <p>{message.timestamp.toLocaleString()}</p>
+    </div>
+  )
 }
-
-const ChatMessage = ({ message, isUser }: chatMessageProps) => {
-    return (
-        <div className={cn("flex flex-col gap-2", isUser ? "items-end" : "items-start")}>
-            <h1>Chat Message</h1>
-        </div>
-    );
-}
-
 
 export default function ChatWidget() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMinimised, setIsMinimised] = useState(false);
+  const [showCloseConfirm, setShowCloseConfirm] = useState(false);
+  const [message, setMessage] = useState<ChatMessage[]>([]);
+  const [input, setInput] = useState("");
+  const [isThinking, setIsThinking] = useState<boolean>(false);
+  const [isGenerating, setIsGenerating] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [lastAcivtityAt, setLastAcivtityAt] = useState<Date | null>(null);
+  const [showSoftReset, setShowSoftReset] = useState<boolean>(false);
+  const [CopiedId, setCopiedId] = useState<string | null>(null);
+  const [feedback, setFeedback] = useState<Record<number, "up" | "down">>({});
+
+  // later add const for CTAs AND AI suggestions 
+  
   return (
     <div className="fixed bottom-4 right-4 w-96 h-full bg-card rounded-lg shadow-lg">
       <h1>Chat Widget</h1>
